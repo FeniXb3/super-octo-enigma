300cb2728bb5566ec68b6f61ba15e252fede0a3d
Konrad Gadzina <konrad@makegames.today>
2024-06-23 13:20:42 +0200

    Use dictionary to configure directions for player

--- super-octo-enigma/Player.cs
+++ super-octo-enigma/Player.cs
@@ -1,29 +1,25 @@
 public class Player : Character
 {
+    private Dictionary<ConsoleKey, Point> directions;
     public Player(string visuals, Point position) : base(visuals, position)
 	{
+        directions = new Dictionary<ConsoleKey, Point>
+        {
+            [ConsoleKey.D] =  new Point(1, 0),
+            [ConsoleKey.A] =  new Point(-1, 0),
+            [ConsoleKey.W] =  new Point(0, -1),
+            [ConsoleKey.S] =  new Point(0, 1),
+        };
     }
 
     protected override Point GetDirection()
     {
         Point point = new Point(0, 0);
         ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-        if (keyInfo.Key == ConsoleKey.D)
-        {
-            point.X += 1;
-        }
-        else if (keyInfo.Key == ConsoleKey.A)
-        {
-            point.X -= 1;
-        }
-        else if (keyInfo.Key == ConsoleKey.W)
-        {
-            point.Y -= 1;
-        }
-        else if (keyInfo.Key == ConsoleKey.S)
-        {
-            point.Y += 1;
-        }
+
+        Point direction = directions.GetValueOrDefault(keyInfo.Key, new Point(0, 0));
+        point.X += direction.X;
+        point.Y += direction.Y;
 
         return point;
     }
